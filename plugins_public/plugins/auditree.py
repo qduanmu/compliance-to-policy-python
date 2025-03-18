7876d4e13a424d8eda0a5a90407f6df457344a6e
Qingmin Duanmu <qduanmu@redhat.com>
2025-03-18 19:05:58 +0800

    chore: update auditree plugin and add test demo
    Signed-off-by: Qingmin Duanmu <qduanmu@redhat.com>

--- plugins_public/plugins/auditree.py
+++ plugins_public/plugins/auditree.py
@@ -15,8 +15,15 @@
 # limitations under the License.
 
 import copy
+import grpc
 import json
+import os
 import re
+import shutil
+import subprocess
+import tempfile
+import yaml
+from concurrent import futures
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
@@ -26,24 +33,38 @@ from pydantic.v1.utils import deep_update
 
 from c2p.common.logging import getLogger
 from c2p.common.utils import get_dict_safely
-from c2p.framework.models import Policy, PVPResult, RawResult
-from c2p.framework.models.pvp_result import (
+from c2p.framework.models import RawResult
+from c2p.framework.plugin_spec import PluginConfig, PluginSpec
+import c2p.framework.api.proto.policy_pb2_grpc as pb2_grpc
+import c2p.framework.api.proto.policy_pb2 as policy_pb2
+from c2p.framework.api.proto.models_pb2 import (
     Link,
     ObservationByCheck,
+    Result,
     PVPResult,
-    ResultEnum,
     Subject,
 )
-from c2p.framework.plugin_spec import PluginConfig, PluginSpec
 
 logger = getLogger(__name__)
 
 status_dictionary = {
-    'pass': ResultEnum.Pass,
-    'fail': ResultEnum.Failure,
-    'warn': ResultEnum.Failure,
-    'error': ResultEnum.Error,
+    'pass': Result.RESULT_PASS,
+    'fail': Result.RESULT_FAILURE,
+    'warn': Result.RESULT_FAILURE,
+    'error': Result.RESULT_ERROR,
 }
+
+
+def update_dict(d, key: Union[str, List[str]], value):
+    if isinstance(key, str):
+        data = copy.deepcopy(d)
+        data[key] = value
+        return data
+    else:
+        update = {key.pop(): value}
+        for _key in reversed(key):
+            update = {_key: update}
+        return deep_update(d, update)
 
 
 class PluginConfigAuditree(PluginConfig):
@@ -57,28 +78,29 @@ class PluginAuditree(PluginSpec):
         super().__init__()
         self.config = config
 
-    def generate_pvp_policy(self, policy: Policy):
+    def generate_pvp_policy(self, policy: policy_pb2.PolicyRequest):
         with Path(self.config.auditree_json_template).open('r') as f:
             auditree_json = json.load(f)
-        parameters = policy.parameters
-        for parameter in parameters:
-            key = parameter.id
-            value = get_dict_safely(auditree_json, key.split('.'))
-            if value is not None:
-                try:
-                    if isinstance(value, list):
-                        updated = parameter.value.split(',')
-                    elif isinstance(value, str):
-                        updated = parameter.value
-                    elif isinstance(value, int):
-                        updated = int(parameter.value)
-                    elif isinstance(value, float):
-                        updated = float(parameter.value)
-                    else:
-                        raise Exception(f'Unsupported parameter value format (parameter_id: {key})')
-                except Exception as e:
-                    raise Exception(f'Invalid parameter value format (parameter_id: {key})') from e
-                auditree_json = update_dict(auditree_json, key.split('.'), updated)
+        for rule in policy.rule:
+            parameter = rule.parameter
+            if parameter:
+                key = parameter.name
+                value = get_dict_safely(auditree_json, key.split('.'))
+                if value is not None:
+                    try:
+                        if isinstance(value, list):
+                            updated = parameter.selected_value.split(',')
+                        elif isinstance(value, str):
+                            updated = parameter.selected_value
+                        elif isinstance(value, int):
+                            updated = int(parameter.selected_value)
+                        elif isinstance(value, float):
+                            updated = float(parameter.selected_value)
+                        else:
+                            raise Exception(f'Unsupported parameter value format (parameter_id: {key})')
+                    except Exception as e:
+                        raise Exception(f'Invalid parameter value format (parameter_id: {key})') from e
+                    auditree_json = update_dict(auditree_json, key.split('.'), updated)
         json.dump(auditree_json, Path(self.config.output).open('w'), indent=2)
 
     def generate_pvp_result(self, raw_result: RawResult) -> PVPResult:
@@ -94,7 +116,7 @@ class PluginAuditree(PluginSpec):
                 observation = ObservationByCheck(
                     check_id=check_id,
                     methods=['AUTOMATED'],
-                    collected=dt,
+                    collected_at=dt,
                 )
 
                 evidences = get_dict_safely(check_class_result, 'evidence', [])
@@ -133,12 +155,12 @@ class PluginAuditree(PluginSpec):
                 subject = Subject(
                     title=f'Auditree Check: {check_id}',
                     type='inventory-item',
-                    result=status_dictionary[status] if status in status_dictionary else ResultEnum.Error,
+                    result=status_dictionary[status] if status in status_dictionary else Result.RESULT_ERROR,
                     resource_id=check_id,
                     evaluated_on=dt,
                     reason=generate_reason(status),
                 )
-                observation.subjects = [subject]
+                observation.subjects.append(subject)
                 observations.append(observation)
 
         # merge observations whose check id is generated by parametrized expansion (see parameterized.expand())
@@ -161,23 +183,102 @@ class PluginAuditree(PluginSpec):
             observation = ObservationByCheck(
                 check_id=normalized_check_id,
                 methods=['AUTOMATED'],
-                collected=group[0].collected,
+                collected_at=group[0].collected_at,
                 subjects=merged_subjects,
             )
             merged_observations.append(observation)
 
-        pvp_result.observations_by_check = merged_observations
+        pvp_result.observations.extend(merged_observations)
 
         return pvp_result
 
 
-def update_dict(d, key: Union[str, List[str]], value):
-    if isinstance(key, str):
-        data = copy.deepcopy(d)
-        data[key] = value
-        return data
-    else:
-        update = {key.pop(): value}
-        for _key in reversed(key):
-            update = {_key: update}
-        return deep_update(d, update)
+class AuditreeServicer(pb2_grpc.PolicyEngineServicer):
+
+    def __init__(self):
+        self.config: Dict = {}
+        self.plugin_id = "Auditree"
+
+    def Generate(
+        self, request: policy_pb2.PolicyRequest, context
+    ) -> policy_pb2.GenerateResponse:
+        """Implemented Generate"""
+        auditree_template = "demo/auditree.template.json"
+        tempdir = tempfile.mkdtemp(prefix="auditree_")
+        generated_auditree_json = os.path.join(tempdir, 'auditree.json')
+        config = PluginConfigAuditree(
+            auditree_json_template=auditree_template, output=generated_auditree_json
+        )
+        generator = PluginAuditree(config)
+        generator.generate_pvp_policy(request)
+        return policy_pb2.GenerateResponse()
+
+    def GetResults(
+        self, request: policy_pb2.PolicyRequest, context
+    ) -> policy_pb2.ResultsResponse:
+        """Implemented GetResults."""
+
+        # Here to run Auditree fetcher and checks using the auditree config created
+        # in the Generate() step and get the check result, check_results.json
+        # Example:
+        # $ compliance --fetch --evidence local -C auditree.json -v
+        # $ compliance --check demo.arboretum.accred,demo.custom.accred --evidence local -C auditree.json -v
+        tmp_path = '/tmp'
+        for d in os.listdir(tmp_path):
+            if d.startswith('auditree'):
+                auditree_json = os.path.join(tmp_path, d, "auditree.json")
+                break
+        command_fetch = [
+                "compliance",
+                "--fetch",
+                "--evidence",
+                "local",
+                "-C",
+                auditree_json,
+        ]
+        subprocess.run(command_fetch, cwd='demo')
+
+        # locker and result are in /tmp/compliance
+        command_check = [
+                "compliance",
+                "--check",
+                "demo.arboretum.accred,demo.custom.accred",
+                "--evidence",
+                "local",
+                "-C",
+                auditree_json,
+        ]
+        subprocess.run(command_check, cwd='demo')
+        shutil.rmtree(os.path.dirname(auditree_json), ignore_errors=True)
+        check_results = '/tmp/compliance/check_results.json'
+        check_results = yaml.safe_load(Path(check_results).open('r'))
+        pvp_raw_result = RawResult(
+            data=check_results,
+            additional_props={
+                'locker_url': 'https://github.com/MY_ORG/MY_EVIDENCE_REPO',
+            },
+        )
+        pvp_result = PluginAuditree().generate_pvp_result(pvp_raw_result)
+        return policy_pb2.ResultsResponse(result=pvp_result)
+
+
+def create_server(uds_address):
+    server = grpc.server(futures.ThreadPoolExecutor())
+    pb2_grpc.add_PolicyEngineServicer_to_server(AuditreeServicer(), server)
+    server.add_insecure_port(uds_address)
+    return server
+
+
+def serve(server):
+    server.start()
+    server.wait_for_termination()
+
+
+def main():
+    uds_address = os.environ.get("UDS_ADDRESS")
+    server = create_server(uds_address)
+    serve(server)
+
+
+if __name__ == "__main__":
+    main()
